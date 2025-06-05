df75f91923ac419baf215c235050e5bb8d57b931
Brandon Kiel <brandonrkiel@gmail.com>
2025-06-04 19:05:13 -0500

    added basic auth

---
+++ src/features/auth/SignInButton.tsx
@@ -0,0 +1,13 @@
+import { signIn } from "@/lib/auth";
+
+export function SignIn() {
+	return (
+		<form
+			action={async () => {
+				"use server";
+				await signIn("github");
+			}}>
+			<button type="submit">Sign in</button>
+		</form>
+	);
+}
---
+++ src/features/auth/SignOutButton.tsx
@@ -0,0 +1,13 @@
+import { signOut } from "@/lib/auth";
+
+export function SignOut() {
+	return (
+		<form
+			action={async () => {
+				"use server";
+				await signOut();
+			}}>
+			<button type="submit">Sign Out</button>
+		</form>
+	);
+}
